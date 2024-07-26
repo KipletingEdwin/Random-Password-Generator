cb1a84b0058ad1aafcdfd240f9a4cf1f9142a4e7
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-26 09:07:25 +0300

    strings will hold the characters/numbers/numbers/symbols

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,2 +1,10 @@
+//this class works as the backend and will generate the password
 public class PasswordGenerator {
+    //character pools
+    //these strings will hold the characters/numbers/numbers/symbols that we are going tpo randomly pick to generate our password
+
+    public  static final String LOWERCASE_CHARACTERS = "qwertyuiopasdfghjklzxcvbnm";
+    public  static  final String UPPERCASE_CHARACTERS = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    public  static  final String NUMBERS = "1234567890";
+    public static final String SPECIAL_SYSMBOLS = ",./;'[]<>?:{}|~!@#$%^&*()_-";
 }
